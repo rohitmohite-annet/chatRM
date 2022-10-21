20bbedead683a7b87e293c3385690d1472703540
rohitmohite-annet <103570246+rohitmohite-annet@users.noreply.github.com>
2022-10-21 15:28:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask
+from flask import Flask, request, render_template,url_for,redirect,jsonify
+import json
+import pandas as pd
+import pyodbc
 
 app = Flask(__name__)
 
@@ -8,12 +11,21 @@ def index():
 
 @app.route('/webhook', methods=['POST'])
 def testwebhook():
-  return {
-        "fulfillmentText": 'This is from the replit webhook',
-        "source": 'webhook'
-    }
+    server = 'sql-4see.database.windows.net'
+    database = 'dev-4see'
+    username = 'db_admin'
+    password = 'Innovation!2022'
+    cnxn = pyodbc.connect(
+        'DRIVER={SQL Server};SERVER=' + server + ';DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
+    cursor = cnxn.cursor()
+    data = pd.read_sql("select Question from DialogFlowSampleQNA", cnxn)
+    first = data.iloc[0, 0]
+
+    return {
+            "fulfillmentText": 'This is from the replit webhook {}'.format(first),
+            "source": 'webhook'
+        }
 
 
 if __name__ == "__main__":
     app.run()
-
