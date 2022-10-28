0231731696f5c17f9613814ab879168b9dd7a677
rohitmohite-annet <103570246+rohitmohite-annet@users.noreply.github.com>
2022-10-28 13:43:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,34 +6,37 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return "<h1>Hello World!</h1>"
-
-def results():
-	# build a request object
-	req = request.get_json(force=True)
-
-	# fetch action from json
-	action = req.get('queryResult').get('action')
-
-	# return a fulfillment response
-	return {'fulfillmentText': 'This is a response from webhook ROHIT.'}
+#
+# def results():
+# 	# build a request object
+# 	req = request.get_json(force=True)
+#
+# 	# fetch action from json
+# 	action = req.get('queryResult').get('action')
+#
+# 	# return a fulfillment response
+# 	return {'fulfillmentText': 'This is a response from webhook ROHIT.'}
 
 
 @app.route('/webhook', methods=['GET','POST'])
 def testwebhook():
-    server = 'sql-4see.database.windows.net'
-    database = 'dev-4see'
-    username = 'db_admin'
-    password = 'Innovation!2022'
-    cnxn = pyodbc.connect(
-        'DRIVER={SQL Server};SERVER=' + server + ';DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
-    cursor = cnxn.cursor()
-    data = pd.read_sql("select Question from DialogFlowSampleQNA", cnxn)
-    first = data.iloc[0, 0]
-    return make_response(jsonify(results()))
-    # return {
-    #         "fulfillmentText": 'This is from the replit webhook {}'.format(first),
-    #         "source": 'webhook'
-    #     }
+    # server = 'sql-4see.database.windows.net'
+    # database = 'dev-4see'
+    # username = 'db_admin'
+    # password = 'Innovation!2022'
+    # cnxn = pyodbc.connect(
+    #     'DRIVER={SQL Server};SERVER=' + server + ';DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
+    # cursor = cnxn.cursor()
+    # data = pd.read_sql("select Question from DialogFlowSampleQNA", cnxn)
+    # first = data.iloc[0, 0]
+    # return make_response(jsonify(results()))
+    req = request.get_json(force=True)
+    session = req.get('session')
+
+    return {
+            "fulfillmentText": str(session) ,
+            "source": 'webhook'
+        }
 
 
 if __name__ == "__main__":
