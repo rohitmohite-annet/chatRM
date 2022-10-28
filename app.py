3cce2e17ec1627da6b65adee143d437531d9d4f0
rohitmohite-annet <103570246+rohitmohite-annet@users.noreply.github.com>
2022-10-28 16:13:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -33,12 +33,11 @@ def testwebhook():
 
     req = request.get_json(force=True)
     queryResult = req.get('queryResult')
-    action = queryResult.get('action')
     queryText = queryResult.get('queryText')
     session = req.get('session')
 
     return {
-            "fulfillmentText": str(req) ,
+            "fulfillmentText": str(queryText) ,
             "source": 'webhook'
         }
 
